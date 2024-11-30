4dd3342e88da3e0e125acf7c8597e8feee02dc2a
lukabrcic <lbrcic4219rn@raf.edu.rs>
2024-11-30 17:26:25 +0100

    Init

---
+++ src/test/java/com/example/fpisfondrum/FpisFondrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fpisfondrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpisFondrumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
