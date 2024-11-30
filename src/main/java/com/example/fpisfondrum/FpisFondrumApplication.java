ced3c2736b889e243475aaf3e1f8f798468f6815
lukabrcic <lbrcic4219rn@raf.edu.rs>
2024-11-30 17:26:25 +0100

    Init

---
+++ src/main/java/com/example/fpisfondrum/FpisFondrumApplication.java
@@ -0,0 +1,13 @@
+package com.example.fpisfondrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpisFondrumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpisFondrumApplication.class, args);
+    }
+
+}
